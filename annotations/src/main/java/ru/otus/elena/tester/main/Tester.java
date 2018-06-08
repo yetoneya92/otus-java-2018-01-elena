4bb9e155f2836c8371b6c953e9cf284f95f2063b
yetoneya92 <36136620+yetoneya92@users.noreply.github.com>
2018-06-08 10:56:54 +0300

    Add files via upload

--- annotations/src/main/java/ru/otus/elena/tester/main/Tester.java
+++ annotations/src/main/java/ru/otus/elena/tester/main/Tester.java
@@ -4,6 +4,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.List;
 import ru.otus.elena.tester.myannotations.After;
 import ru.otus.elena.tester.myannotations.Before;
 import ru.otus.elena.tester.myannotations.Test;
@@ -14,71 +15,73 @@ public class Tester {
         
         ArrayList<String> list = new ArrayList<>();
         if (clazz == null) {
-            list.add("tests not exist");
+            list.add("tests don't exist");
             return list;
-        }   
+        }
         Method[] methods = clazz.getMethods();
-        if(methods.length==0) {
-            list.add("tests not exist");
+        if (methods.length == 0) {
+            list.add("tests don't exist");
             return list;
         }
         try {
-        Annotation annotation = null;
-        Object obj = null;
-        
+            List<Method> beforeMethods = new ArrayList<>();
+            List<Method> testMethods = new ArrayList<>();
+            List<Method> afterMethods = new ArrayList<>();
+
             s1:
             for (Method m : methods) {
-                StringBuilder result = new StringBuilder();
-                annotation = m.getAnnotation(Test.class);
-                if (annotation == null) {
+                Annotation b = m.getAnnotation(Before.class);
+                Annotation t = m.getAnnotation(Test.class);
+                Annotation a = m.getAnnotation(After.class);
+                if (b != null) {
+                    beforeMethods.add(m);
+                    continue s1;
+                } else if (t != null) {
+                    testMethods.add(m);
+                    continue s1;
+                } else if (a != null) {
+                    afterMethods.add(m);
                     continue s1;
                 }
-                obj = clazz.newInstance();
-                s2:
-                for (Method n : methods) {
-                    annotation = n.getAnnotation(Before.class);
-                    if (annotation != null) {
-                        try {
-                            n.invoke(obj);
-                        } catch (InvocationTargetException ite) {
-                            result.append(n.getName() + " = Test failed: " + ite.getCause().getMessage());
-                            break s1;
-                        }
-                        try {
-                            result.append(m.getName()).append(" = ");
-                            m.invoke(obj);
-                            result.append("success  ");                            
-            
-                            break s2;
-                        } catch (InvocationTargetException ite) {
-                            result.append("Test failed: " + ite.getCause().getMessage()).append("  ");
-                            break s2;
-                        }
-
-                    }
-                }
-                s3:
-                for (Method n : methods) {
-                     annotation = n.getAnnotation(After.class);
-                    if (annotation != null) {
-                        try {
-                            n.invoke(obj);
-                            list.add(result.toString());
-                        } catch (InvocationTargetException ite) {
-                            result.append(n.getName() + " = Test failed: " + ite.getCause().getMessage());
-                            break s1;
-                        }
-                    }
-
-                }
-
             }
-        } catch (Exception e) {
+            s2:
+            for (Method m : testMethods) {
+                StringBuilder result = new StringBuilder();
+                Object obj = clazz.newInstance();
+                for (Method n : beforeMethods) {
+                    try {
+                        n.invoke(obj);
+                    } catch (InvocationTargetException ite) {
+                        result.append(n.getName() + " = Test failed: " + ite.getCause().getMessage());
+                        break s2;
+                    }
+                }
+                try {
+                    result.append(m.getName()).append(" = ");
+                    m.invoke(obj);
+                    result.append("success");
+                } catch (InvocationTargetException ite) {
+                    result.append("Test failed: " + ite.getCause().getMessage());
+                }
+                for (Method n : afterMethods) {
+                    try {
+                        n.invoke(obj);
+                        list.add(result.toString());                        
+                    } catch (InvocationTargetException ite) {
+                        result.append(n.getName() + " = Test failed: " + ite.getCause().getMessage());
+                        break s2;
+                    }
+                }
+            }
+            
+        }catch(Exception e) {
             //System.out.println(e.getMessage());
             list.add(e.getMessage());
             return list;
         }
         return list;
     }
-}   
+}    
+            
+
  
